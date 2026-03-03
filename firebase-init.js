910cc7914c24e3b7fada44cf4cdc7779d53272d7
atmaprayer <atmaprayer@gmail.com>
2026-03-02 20:35:40 -0500

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -6,10 +6,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-aut
 // 1) Replace the object below with your Firebase Web App config (Firebase Console → Project settings → Your apps)
 // 2) Make sure your GitHub Pages domain (YOURNAME.github.io) is added in Auth → Settings → Authorized domains
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_AUTH_DOMAIN",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  appId: "PASTE_YOUR_APP_ID",
+  apiKey: "AIzaSyAVVRzI90Ry14Aj5G-6fv82EDXNoeo09ec",
+  authDomain: "atma-app-30e21.firebaseapp.com",
+  projectId: "atma-app-30e21",
+  storageBucket: "atma-app-30e21.firebasestorage.app",
+  messagingSenderId: "401057661341",
+  appId: "1:401057661341:web:58ed3573bfa9200fa5af74",
+  measurementId: "G-JB2Q1DKEER"
 };
 
 export const app = initializeApp(firebaseConfig);
